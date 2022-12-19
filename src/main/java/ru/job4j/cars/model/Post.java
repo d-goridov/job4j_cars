a2d5b8ea66e8eaa2b1e0d2f3eedd6079c3752d4a
d.goridov <dimagoridov@yandex.ru>
2022-12-19 23:01:41 +0300

    1. ToMany [#301848]

---
+++ src/main/java/ru/job4j/cars/model/Post.java
@@ -0,0 +1,28 @@
+package ru.job4j.cars.model;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Data
+@Entity
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Table(name = "auto_post")
+public class Post {
+    @Id
+    @EqualsAndHashCode.Include
+    @Column(name = "id")
+    private int id;
+    @Column(name = "description")
+    private String description;
+    @Column(name = "created")
+    private LocalDateTime created;
+    @OneToMany(cascade = CascadeType.ALL)
+    @JoinColumn(name = "auto_post_id")
+    private List<PriceHistory> priceHistories;
+    @ManyToOne
+    @JoinColumn(name = "auto_user_id")
+    private User user;
+}
---
+++ src/main/java/ru/job4j/cars/model/PriceHistory.java
@@ -0,0 +1,29 @@
+package ru.job4j.cars.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Data
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "price_history")
+public class PriceHistory {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @EqualsAndHashCode.Include
+    @Column(name = "id")
+    private int id;
+    @Column(name = "before")
+    private int before;
+    @Column(name = "after")
+    private int after;
+    @Column(name = "created")
+    private LocalDateTime created;
+}
